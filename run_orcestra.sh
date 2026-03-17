05b5f1cdef820767b61f47a3ee2e97eeb1a77ec9
Rama <zulkiflirmdn@gmail.com>
2026-03-18 06:16:05 +1100

    Update run_orcestra.sh to increase CPU allocation from 8 to 16

--- run_orcestra.sh
+++ run_orcestra.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #PBS -N OrcestraProcessing
 #PBS -q normal
-#PBS -l ncpus=8
+#PBS -l ncpus=16
 #PBS -l mem=32GB
 #PBS -l walltime=05:00:00
 #PBS -l storage=gdata/k10+home/zr7147
 #PBS -l wd
 
 # Load your environment
-source /home/565/zr7147/.bashrc
 conda activate /g/data/k10/zr7147/orcestra_env
 
 # Run your analysis
