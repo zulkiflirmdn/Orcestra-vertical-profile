85647ea91fa9b69e89f62c23595d0009adbec305
Rama <zulkiflirmdn@gmail.com>
2026-04-09 18:57:50 +1000

    Reorganise: move scripts to scripts/, docs to notebooks/, ignore log/notes/sh_script, remove vscode job logs

--- comparison_plotting.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Backward-compatible wrapper for the comparison plotting module."""
-
-from scripts.comparison_plotting import main
-
-
-if __name__ == "__main__":
-    main()
--- earthcare_preprocessing.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Backward-compatible wrapper for the EarthCARE preprocessing module."""
-
-from scripts.earthcare_preprocessing import main
-
-
-if __name__ == "__main__":
-    main()
